6f3ca31ba50ec63cef67bd67b27ebfcba538c5ee
capemo <camilo_p32@hotmail.com>
2016-05-22 19:50:02 -0500

    Login Page Completed

--- Projects/uebgate/Handler/UEBGateH.hs
+++ Projects/uebgate/Handler/UEBGateH.hs
@@ -8,28 +8,41 @@ getUEBGateHR = do
     defaultLayout $ do 
         -- $(widgetFile "files")
         setTitle "Welcome to UEBGate"
-        toWidget [lucius| h1 { color: green; } |]
         addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
         toWidget
-            [julius|
-                $(function() {
-                    $("h1").click(function(){
-                        alert("You clicked on the heading!");
-                    });
-                });
-            |]
-        toWidgetHead
-            [hamlet|
-                <meta name=keywords content="some sample keywords">
+            [lucius|
+                #login-container {
+                    position: fixed;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }
             |]
         toWidget
-            [hamlet|
-                <h1>Here's one way of including content
-                
+            [lucius|
+                body {
+                    background-image:url(@{StaticR uebgate_png});
+                }
             |]
-        [whamlet|<h2>Here's another |]
         toWidgetBody
-            [julius|
-                alert("This is included in the body itself");
+            [hamlet|
+            <div id="login-container" class="container">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="login-panel panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title" style="text-align:center;">UEBGate Sign In
+                            <div class="panel-body">
+                                <form role="form">
+                                    <fieldset>
+                                        <div class="form-group">
+                                            <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                        <div class="form-group">
+                                            <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                        <!-- Change this to a button or input when using this as a form -->
+                                        <a href="index.html" class="btn btn-lg btn-success btn-block">Login
             |]
         $(widgetFile "home/uebgateh")
